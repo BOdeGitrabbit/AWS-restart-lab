8e16b2bcaf6696f84c785c4c1e746e33d856a446
BOdeGitrabbit <106731496+BOdeGitrabbit@users.noreply.github.com>
2022-06-03 00:14:08 +0800

    Add files via upload

---
+++ AWS lab 4.py
@@ -0,0 +1,35 @@
+
+myFruitList = ["apple", "banana", "cherry"]
+print("Lab 4 - Exercise 1: Introducing the list data type")
+print(myFruitList)
+print(type(myFruitList))
+print(myFruitList[0])
+print(myFruitList[1])
+print(myFruitList[2])
+
+myFruitList[2] = "orange"
+print(myFruitList)
+
+print("                                                    ")
+print("Lab 4 - Exercise 2: Introducing the tuple data type")
+myFinalAnswerTuple = ("apple", "banana", "pineapple")
+print(myFinalAnswerTuple)
+print(type(myFinalAnswerTuple))
+print(myFinalAnswerTuple[0])
+print(myFinalAnswerTuple[1])
+print(myFinalAnswerTuple[2])
+
+print("                                                    ")
+print("Lab 4 - Exercise 3: Introducing the dictionary data type")
+
+myFavoriteFruitDictionary = {
+  "Akua" : "apple",
+  "Saanvi" : "banana",
+  "Paulo" : "pineapple"
+}
+
+print(myFavoriteFruitDictionary)
+print(type(myFavoriteFruitDictionary))
+print(myFavoriteFruitDictionary["Akua"])
+print(myFavoriteFruitDictionary["Saanvi"])
+print(myFavoriteFruitDictionary["Paulo"])
