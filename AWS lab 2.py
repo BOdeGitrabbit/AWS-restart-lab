32d5130eb601bc070d374f42a0c74c0766b47c76
BOdeGitrabbit <106731496+BOdeGitrabbit@users.noreply.github.com>
2022-06-02 22:31:08 +0800

    Add files via upload

---
+++ AWS lab 2.py
@@ -0,0 +1,44 @@
+a=2+2
+b=4-2
+c=2*2
+d=int(4/2)
+
+print("                                                         ")
+print("Lab 2- Exercise 1")
+print("Testing the Plus, Minus, Multiply, Division ")
+print("2=2="+str(a))
+print("4-2="+str(b))
+print("2*2="+str(c))
+print("4/2="+str(d))
+
+print("                                                         ")
+print("Lab 2- Exercise 2")
+print("Python has three numeric types: int, float, and complex")
+myValue=1
+print(myValue)
+print(type(myValue))
+print(str(myValue) + " is of the data type " + str(type(myValue)))
+
+print("                                                         ")
+print("Lab 2- Exercise 3")
+myValue=3.14
+print(myValue)
+print(type(myValue))
+print(str(myValue) + " is of the data type " + str(type(myValue)))
+
+print("                                                         ")
+print("Lab 2- Exercise 4")
+myValue=5j
+print(myValue)
+print(type(myValue))
+print(str(myValue) + " is of the data type " + str(type(myValue)))
+
+print("                                                         ")
+myValue=True
+print(myValue)
+print(type(myValue))
+print(str(myValue) + " is of the data type " + str(type(myValue)))
+myValue=False
+print(myValue)
+print(type(myValue))
+print(str(myValue) + " is of the data type " + str(type(myValue)))
